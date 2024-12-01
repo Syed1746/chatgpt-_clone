5c36b6e64b3aff6bd369e14a22632c78597a0ffe
syed1746 <syedsameernazeer@gmail.com>
2024-12-01 22:34:15 +0530

    projec setup has been done

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const colors = require('colors')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    console.log("get requrest server")
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`server is listening on port : ${process.env.PORT} and ${process.env.DEV_MODE}`)
+})
